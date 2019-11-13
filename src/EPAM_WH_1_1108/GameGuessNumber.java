e9382aa6bcf6b6e707f49131952d28feae251941
Vladyslav-Fr <tertey7@gmail.com>
2019-11-13 22:43:33 +0200

    minor improvements for test

--- src/EPAM_WH_1_1108/GameGuessNumber.java
+++ src/EPAM_WH_1_1108/GameGuessNumber.java
@@ -4,40 +4,51 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GameGuessNumber {
+    private static final String GUESS_AND_ENTER = "Guess the number game. \nEnter an integer number from 0 to 1000: ";
+    private static final String TO_HIGH = "Too high. Enter next an integer number: ";
+    private static final String TO_LOW = "Too low. Enter next an integer number: ";
+    private static final String WARMER_BUT_HIGH = "Warmer but high. Enter next an integer number: ";
+    private static final String WARMER_BUT_LOW = "Warmer but low. Enter next an integer number: ";
+    private static final String CONGRATS = "\nGotcha! Congrats!!! \nThe hidden number is: ";
+    private static final String ATTEMPTS = "Number of attempts is: ";
+    private static final String WRONG = "You are wrong! It's not an integer. Please re-enter the correct number: ";
 
     public static void main(String[] args) {
-        int ranNum, enterNum, attempts = 0;
-
         GameGuessNumber gameGuessNumber = new GameGuessNumber();
-        ranNum = gameGuessNumber.randomIntNum0_1000();
+        gameGuessNumber.game();
+    }
 
-        System.out.print("Guess the number game. \nEnter an integer number from 0 to 1000: ");
+    private void game() {
+        int ranNum, enterNum, attempts = 0, warmer = 10;
+
+        ranNum = randomIntNumFrom0to1000();
+
+        System.out.print(GUESS_AND_ENTER);
         do {
-            enterNum = gameGuessNumber.setNum();
+            enterNum = setNum();
             if (enterNum > ranNum) {
-                if ((enterNum - ranNum) > 10) {
-                    System.out.print("Too high. Enter next an integer number: ");
+                if ((enterNum - ranNum) > warmer) {
+                    System.out.print(TO_HIGH);
                 } else {
-                    System.out.print("Warmer but high. Enter next an integer number: ");
+                    System.out.print(WARMER_BUT_HIGH);
                 }
             } else if (enterNum < ranNum) {
-                if ((ranNum - enterNum) > 10) {
-                    System.out.print("Too low. Enter next an integer number: ");
+                if ((ranNum - enterNum) > warmer) {
+                    System.out.print(TO_LOW);
                 } else {
-                    System.out.print("Warmer but low. Enter next an integer number: ");
+                    System.out.print(WARMER_BUT_LOW);
                 }
             } else {
-                System.out.print("Gotcha! Congrats!!! \nThe hidden number is: " + ranNum);
+                System.out.print(CONGRATS + ranNum);
             }
             attempts++;
 
         } while (enterNum != ranNum);
 
-        System.out.println("Number of attempts is: " + attempts);
-
+        System.out.println(ATTEMPTS + attempts);
     }
 
-    private int randomIntNum0_1000() {
+    private int randomIntNumFrom0to1000() {
         Random random = new Random();
         return random.nextInt(1000);
     }
@@ -47,7 +58,7 @@ public class GameGuessNumber {
         if (sc.hasNextInt()) {
             return sc.nextInt();
         } else {
-            System.out.println("You are wrong! It's not an integer. Please re-enter the correct number.");
+            System.out.print(WRONG);
             return setNum();
         }
     }
