7e93f00d4c6b9cc28a205e7b33c336147cdc2c64
Vladyslav-Fr <tertey7@gmail.com>
2019-12-01 14:52:07 +0200

    minor refactoring

--- src/main/java/info/vladyslav/EPAM_HW_5_1122/T1RotateLinkedList.java
+++ src/main/java/info/vladyslav/EPAM_HW_5_1122/T1RotateLinkedList.java
@@ -30,10 +30,10 @@ public class T1RotateLinkedList {
 
     void shiftRight(int k) {
         for (int j = 0; j < k; j++) {
-            Integer temporaryStorageForOneDigitToMove = rotor.get(rotor.size() - 1);
+            Integer forOneDigitToMove = rotor.get(rotor.size() - 1);
             for (int i = rotor.size() - 1; i >= 0; i--) {
                 if (i == 0) {
-                    rotor.set(i, temporaryStorageForOneDigitToMove);
+                    rotor.set(i, forOneDigitToMove);
                 } else {
                     rotor.set(i, rotor.get(i - 1));
                 }
