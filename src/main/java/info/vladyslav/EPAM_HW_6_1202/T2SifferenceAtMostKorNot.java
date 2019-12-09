977185e2140bbebe5e40013f8764d6d5bb38fdb5
Vladyslav-Fr <tertey7@gmail.com>
2019-12-09 09:39:21 +0200

    initial commit

---
+++ src/main/java/info/vladyslav/EPAM_HW_6_1202/T2SifferenceAtMostKorNot.java
@@ -0,0 +1,29 @@
+package info.vladyslav.EPAM_HW_6_1202;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Task 2
+ * Given an array of integers and an integer k, find out whether there are
+ * two distinct indices i and j in the array such that nums[i] = nums[j]
+ * and the absolute difference between i and j is at most k.
+ */
+
+public class T2SifferenceAtMostKorNot {
+
+    boolean containsNearbyDuplicate(LinkedList<Integer> linkedList, int k) {
+        HashMap<Integer, Integer> hashMapForCompare = new HashMap<>();
+
+        for (int i = 0; i < linkedList.size(); i++) {
+            if (hashMapForCompare.containsKey(linkedList.get(i))) {
+                int duplicateKeyInitialIndex = hashMapForCompare.get(linkedList.get(i));
+                System.out.println(hashMapForCompare.get(linkedList.get(i)));
+                if (i - duplicateKeyInitialIndex <= k)
+                    return true;
+            }
+            hashMapForCompare.put(linkedList.get(i), i);
+        }
+        return false;
+    }
+}
---
+++ src/test/java/info/vladyslav/EPAM_HW_6_1202/T3AmountKindsOfCandiesTest.java
@@ -0,0 +1,40 @@
+package info.vladyslav.EPAM_HW_6_1202;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class T3AmountKindsOfCandiesTest {
+T3AmountKindsOfCandies t3AmountKindsOfCandies = new T3AmountKindsOfCandies();
+ArrayList<Integer> integers = new ArrayList<>();
+    @Test
+    public void maxAmountOfKindOfCandies1() {
+        integers.add(1);
+        integers.add(1);
+        integers.add(2);
+        integers.add(2);
+        integers.add(3);
+        integers.add(3);
+        assertEquals(t3AmountKindsOfCandies.maxAmountOfKindOfCandies(integers), 3);
+    }
+
+    @Test
+    public void maxAmountOfKindOfCandies2() {
+        integers.add(1);
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        assertEquals(t3AmountKindsOfCandies.maxAmountOfKindOfCandies(integers), 2);
+    }
+
+    @Test
+    public void maxAmountOfKindOfCandies3() {
+        integers.add(1);
+        integers.add(1);
+        integers.add(1);
+        integers.add(1);
+        assertEquals(t3AmountKindsOfCandies.maxAmountOfKindOfCandies(integers), 1);
+    }
+}
