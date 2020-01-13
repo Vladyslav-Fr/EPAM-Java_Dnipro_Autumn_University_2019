f8f722e7b776f2f5542c6af4915c5d7d95ab6b57
Vladyslav-Fr <tertey7@gmail.com>
2020-01-13 10:22:58 +0200

    initial commit

--- src/main/java/info/vladyslav/EPAM_HW_10_1230/Task 6.sql
+++ src/main/java/info/vladyslav/EPAM_HW_10_1230/Task 6.sql
@@ -1,26 +1,26 @@
-CREATE TABLE IF NOT EXISTS `epam`.`courses`
+CREATE TABLE IF NOT EXISTS `courses`
 (
     `student` VARCHAR(255) NOT NULL,
     `class`   VARCHAR(255) NULL,
     PRIMARY KEY (`student`)
 );
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('A', 'Math');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('B', 'English');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('C', 'Math');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('D', 'Biology');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('E', 'Math');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('F', 'Computer');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('G', 'Math');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('H', 'Math');
-INSERT INTO `epam`.`courses` (`student`, `class`)
+INSERT INTO `courses` (`student`, `class`)
 VALUES ('I', 'Math');
 
 SELECT Class
