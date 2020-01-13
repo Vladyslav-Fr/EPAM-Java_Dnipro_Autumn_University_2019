57953813dae2ea4ca70ae624dd4f0ae6958e185b
Vladyslav-Fr <tertey7@gmail.com>
2020-01-13 10:04:48 +0200

    initial commit

--- src/main/java/info/vladyslav/EPAM_HW_10_1230/Task 4.sql
+++ src/main/java/info/vladyslav/EPAM_HW_10_1230/Task 4.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `person`
+CREATE TABLE IF NOT EXISTS `person`
 (
     `Id`    INT          NOT NULL,
     `Email` VARCHAR(255) NULL,
---
+++ src/main/java/info/vladyslav/EPAM_HW_10_1230/Task 5.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `epam`.`customers`
+(
+    `Id`   INT          NOT NULL,
+    `Name` VARCHAR(255) NULL,
+    PRIMARY KEY (`Id`)
+);
+CREATE TABLE `epam`.`orders`
+(
+    `Id`         INT         NOT NULL,
+    `CustomerId` VARCHAR(45) NULL,
+    PRIMARY KEY (`Id`)
+);
+
+INSERT INTO `epam`.`customers` (`Id`, `Name`)
+VALUES ('1', 'Joe');
+INSERT INTO `epam`.`customers` (`Id`, `Name`)
+VALUES ('2', 'Henry');
+INSERT INTO `epam`.`customers` (`Id`, `Name`)
+VALUES ('3', 'Sam');
+INSERT INTO `epam`.`customers` (`Id`, `Name`)
+VALUES ('4', 'Max');
+
+INSERT INTO `epam`.`orders` (`Id`, `CustomerId`)
+VALUES ('1', '3');
+INSERT INTO `epam`.`orders` (`Id`, `CustomerId`)
+VALUES ('2', '1');
+
+SELECT Name
+FROM customers c
+WHERE Id NOT IN (SELECT CustomerId FROM Orders);
